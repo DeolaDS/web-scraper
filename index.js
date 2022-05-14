17fdbc93c16e5d45d910174efca69beed8156770
deola <deola.c.dsilva3@gmail.com>
2022-05-14 18:28:46 +0400

    scraping with axios, cheerio, nodejs

--- index.js
+++ index.js
@@ -9,7 +9,33 @@ const PORT = 8000 /**Want our server to run on this port */
 
 const app = express()
 
+const url ="https://www.tripadvisor.com/Attractions-g295424-Activities-c26-t143-Dubai_Emirate_of_Dubai.html"
+
+axios(url)
+/**chaining */
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles =[]
+        
+        //find this class in html, and for each
+        $('.eHyiI', html).each(function(){ //function cant be an expression 
+            //get the text 
+            const title = $(this).text()
+
+            //get the link, first the a tag and in that, get the href value
+            //const link = $(this).find('a').attr('href')
+            articles.push({ //add an object with title and link into the array
+                title
+                //link
+            })
+        })
+        console.log(articles)
+    }).catch(err => console.log(err))
+
 /* List to PORT and see if any changes are made 
 () Callback */
 
-app.listen(PORT, ()=> console.log("Server running on ${PORT}"))
+app.listen(PORT, () => console.log('server running on Port '+ PORT))
+
+/** Use npm run start bc the script is named that */
